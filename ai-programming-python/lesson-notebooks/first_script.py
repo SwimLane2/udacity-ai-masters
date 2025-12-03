c6ca73482089a3d788626eccb2aa066af2d72d54
Arjun Avalakki Mohan <59450852+SwimLane2@users.noreply.github.com>
2025-12-03 09:26:28 +0000

    Editing the first script file

--- ai-programming-python/lesson-notebooks/first_script.py
+++ ai-programming-python/lesson-notebooks/first_script.py
@@ -1,1 +1,7 @@
-print('Congratulations on running this script!!')
+new_content = """
+print("This is my edited script!")
+print("Python is working through Colab!")
+"""
+
+with open('first_script.py', 'w') as f:
+    f.write(new_content)
