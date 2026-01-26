fc4c0c8d32e3f32daa6d585548ae2386da1074c9
Arjun Avalakki Mohan <59450852+SwimLane2@users.noreply.github.com>
2026-01-26 06:33:30 +0000

    completed task 2

--- Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 8/Caesar Cipher 2/main.py
+++ Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 8/Caesar Cipher 2/main.py
@@ -6,10 +6,20 @@ shift = int(input("Type the shift number:\n"))
 
 
 # TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
+def decrypt(original_text, shift_amount):
+    decrypted_text = ""
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) - shift_amount #alphabet.index(letter) gets the index of the letter in the alphabet list
+        shifted_position %= len(alphabet)
+        # You can build the decrypted text here similar to the encrypt function
+        # and print it out at the end.
+        decrypted_text += alphabet[shifted_position]
+    return decrypted_text
+
 # TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
 #  by the shift amount and print the decrypted text.
-# TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
-#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
+
+
 
 def encrypt(original_text, shift_amount):
     cipher_text = ""
@@ -17,10 +27,22 @@ def encrypt(original_text, shift_amount):
         shifted_position = alphabet.index(letter) + shift_amount
         shifted_position %= len(alphabet)
         cipher_text += alphabet[shifted_position]
-    print(f"Here is the encoded result: {cipher_text}")
+    return cipher_text
+    
 
 
-encrypt(original_text=text, shift_amount=shift)
+# TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
+#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
+def caesar(original_text, shift_amount, chosen_direction):
+    if chosen_direction == "encode":
+        result = encrypt(original_text, shift_amount)
+    elif chosen_direction == "decode":
+        result = decrypt(original_text, shift_amount)
+    return result
+
+final_text = caesar(original_text=text, shift_amount=shift, chosen_direction=direction)
+print(f"The {direction}d text is: {final_text}")# TODO-4: Call the 'caesar()' function and pass in the 'text', 'shift' and 'direction' variables.
+
 
 
 
