ac9679072155e265bdd8c9d5a70c5520256d6859
Arjun Avalakki Mohan <59450852+SwimLane2@users.noreply.github.com>
2026-01-28 06:21:30 +0000

    completed day 9 tasks and project

--- Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 9/Blind Auction Project/main.py
+++ Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 9/Blind Auction Project/main.py
@@ -1,3 +1,25 @@
+
+import art
+print(art.logo)
+
+import os
+clear = lambda: os.system('cls')
+
+bid_data = {}
+bidder = True
+while bidder:
+    name = str(input("What is your name?: "))
+    price = int(input("What is your bid?: £ "))
+    other_bidders = str(input("Are there any other bidders? Type 'yes' or 'no'. "))
+    bid_data[name] = price
+    if other_bidders == "no":
+        bidder = False
+        highest_bidder_name = max(bid_data, key=bid_data.get)
+        highest_bidder_amount = bid_data[highest_bidder_name]
+        print(f"The winner is {highest_bidder_name} with a bid of £{highest_bidder_amount}")
+    else:
+        clear()
+
 # TODO-1: Ask the user for input
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
---
+++ Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 9/Nested Lists and Dictionaries/task.py
@@ -0,0 +1,50 @@
+capital = {
+    "France": "Paris",
+    }
+
+# travel_log = {
+#     "France": ["Paris", "Lille", "Dijon"],
+#     "Germany": ["Stuttgart", "Berlin"],
+# }
+
+# #print Lille
+# print(travel_log["France"][1])
+
+# nested_list = ["A", "B", ["C", "D"]]
+# print(nested_list[2][0])
+
+# travel_log = {
+#   "France": {
+#     "cities_visited": ["Paris", "Lille", "Dijon"], 
+#     "total_visits": 12
+#    },
+#   "Germany": {
+#     "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], 
+#     "total_visits": 5
+#    },
+# }
+
+# print(travel_log["Germany"]["cities_visited"][2])
+
+# starting_dictionary = {
+#     "a": 9,
+#     "b": 8,
+# }
+
+# starting_dictionary["c"] = 7
+# print(starting_dictionary)
+
+dict = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+}
+#dict["c"] = [1, 2, 3]
+# for key in dict:
+#     dict[key] += 1
+#     print(key)
+
+dict[1] = 4
+# print(dict)
+#print(dict)
+print(dict[1])
