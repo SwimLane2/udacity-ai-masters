aa2f8e03701d84855ee13869edff1dd967c86ada
Arjun Avalakki Mohan <59450852+SwimLane2@users.noreply.github.com>
2026-01-29 06:41:18 +0000

    day 10 work so far

--- Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 10/Calculator Project/main.py
+++ Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 10/Calculator Project/main.py
@@ -1,4 +1,21 @@
 def add(n1, n2):
     return n1 + n2
 
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+mathematical_operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+print(mathematical_operations["*"](n1=4, n2=8))
 
---
+++ Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 10/Functions with Outputs/task.py
@@ -0,0 +1,7 @@
+def format_name(f_name, l_name):
+    format_firstname = f_name.title()
+    format_lastname = l_name.title()
+    
+    return print(f"{format_firstname} {format_lastname}")
+
+format_name("arjun", "ARJUN")
