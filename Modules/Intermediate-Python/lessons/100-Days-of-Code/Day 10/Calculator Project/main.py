ba647bd0eac290e3599c92907d7f0b9a4ad7a282
Arjun Avalakki Mohan <59450852+SwimLane2@users.noreply.github.com>
2026-01-30 06:09:26 +0000

    completed day 10 work

--- Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 10/Calculator Project/main.py
+++ Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 10/Calculator Project/main.py
@@ -1,3 +1,6 @@
+import art
+
+
 def add(n1, n2):
     return n1 + n2
 
@@ -17,5 +20,40 @@ mathematical_operations = {
     "/": divide
 }
 
-print(mathematical_operations["*"](n1=4, n2=8))
+def display_operators():
+    for key in mathematical_operations:
+        print(key)
+
+
+def calculate():
+    print(art.logo)
+    calculation_loop = True
+    first_number = float(input("What's the first number?: "))
+    while calculation_loop:
+        display_operators()
+        user_operator = input("Pick an operation: ")
+        next_number = float(input("What's the next number?: "))
+        calculate_results = mathematical_operations[user_operator](n1=first_number, n2=next_number)
+        print(f"{first_number} {user_operator} {next_number} = {calculate_results}")
+        continue_calculation = input(f"Type 'y' to continue calculating with {calculate_results}, or type 'n' to start a new calculation: ").lower()
+        if continue_calculation == "y":
+            first_number = calculate_results
+        elif continue_calculation == "n":
+            calculation_loop = True
+            calculate()
+
+calculate()
+
+
+
+
+
+        # first_number = calculate_results
+        # display_operators()
+        # user_operator = input("Pick an operation: ")
+        # next_number = float(input("What's the next number?: "))
+        # calculate_results = mathematical_operations[user_operator](n1=first_number, n2=next_number)
+        # print(f"{first_number} {user_operator} {next_number} = {calculate_results}")
+        # continue_calculation = input(f"Type 'y' to continue calculating with {calculate_results}, or type 'n' to start a new calculation: ").lower()
+#print(mathematical_operations["*"](n1=4, n2=8))
 
