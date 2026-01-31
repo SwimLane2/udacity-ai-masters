c21262fa2a628de1d5aada7ecf790cf1cf5d4dde
Arjun Avalakki Mohan <59450852+SwimLane2@users.noreply.github.com>
2026-01-31 07:15:46 +0000

    changes

--- Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 11/task/main.py
+++ Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 11/task/main.py
@@ -8,34 +8,49 @@ def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     return random.choice(cards)
 
-def calculate_score(cards):
-    total = sum(cards)
-    return total
+def calculate_score(deal_cards):
+    total = sum(deal_cards)
+    #check for blackjack with two cards only
+    if len(deal_cards) == 2 and total == 21:
+        return 0  # special blackjack signal
 
+    if 11 in deal_cards and total > 21:
+        deal_cards.remove(11)
+        deal_cards.append(1)
+        total = sum(deal_cards)
+    return total
 
 is_game_over = True
 while is_game_over:
     start_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
     if start_game == 'y':
         print(art.logo)
-        user_cards = []
-        computer_cards = []
+        user = []
+        computer = []
                
         #1. Deal both user and computer a starting hand of 2 random card values.
         for _ in range(2):
-            user_cards.append(deal_card())
-            computer_cards.append(deal_card())
+            user.append(deal_card())
+            computer.append(deal_card())
 
         #2. Adding the values
-        adding_user_cards = calculate_score(user_cards)
-        adding_computer_cards = sum(computer_cards)
-        comp_first_card = computer_cards[0]
-
-        print(f"Your cards: {user_cards}, current score: {adding_user_cards}")
+        user_score = calculate_score(user)
+        computer_score = calculate_score(computer)
+        comp_first_card = computer[0]
+        print(f"Your cards: {user}, current score: {user_score}")
         print(f"Computer's first card: {comp_first_card}")
 
+        #3. Does the user or computer have a blackjack
+        if user_score == 0 or computer_score == 0:
+            if user_score == 0:
+                print("You win 😁")
+            elif computer_score:
+                print("You lose 😤")
+        elif user_score > 21:
+            
 
-        #2. Detect when computer or user has a blackjack. (Ace + 10 value card).
+        
+
 
 
     
