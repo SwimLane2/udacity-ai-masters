2ad65a73f5e4025c939d14292d2a5c700569d2a6
Arjun Avalakki Mohan <arjun.avalakki@gmail.com>
2026-02-04 06:39:21 +0000

    new exercise update

---
+++ Modules/Intermediate-Python/exercises/map-filter/exercise.py
@@ -0,0 +1,16 @@
+# Practice with map
+# Fill out the rest of the map functions.
+# You can define additional functions if you need to.
+# (a) ["apple", "orange", "pear"] => (5, 6, 4)  (length)
+# (b) ["apple", "orange", "pear"] => ("APPLE", "ORANGE", "PEAR")  (uppercase)
+# (c) ["apple", "orange", "pear"] => ("elppa", "egnaro", "raep")  (reversed)
+# (d) ["apple", "orange", "pear"] => ("ap", "or", "pe")  (first two letters)
+
+word = ["apple", "orange", "pear"]
+
+a = map(len, word)
+
+# b = map(???, ???)
+# c = map(???, ???)
+# d = map(???, ???)
+print(list(a))
