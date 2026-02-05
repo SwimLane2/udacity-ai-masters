249a3cc26c124b7f3b97facbe398e47de3adee93
Arjun Avalakki Mohan <arjun.avalakki@gmail.com>
2026-02-05 05:25:09 +0000

    map-filter exercise completed

--- Modules/Intermediate-Python/exercises/map-filter/exercise.py
+++ Modules/Intermediate-Python/exercises/map-filter/exercise.py
@@ -6,13 +6,32 @@
 # (c) ["apple", "orange", "pear"] => ("elppa", "egnaro", "raep")  (reversed)
 # (d) ["apple", "orange", "pear"] => ("ap", "or", "pe")  (first two letters)
 
-word = ["apple", "orange", "pear"]
+# word = ["apple", "orange", "pear"]
 
-a = map(len, word)
-b = map(str.upper, word)
-c = map(lambda s: s[::-1], word)
-d = map(lambda s: s[:2], word)
-print(list(a))
-print(list(b))
-print(list(c))
+# a = map(len, word)
+# b = map(str.upper, word)
+# c = map(lambda s: s[::-1], word)
+# d = map(lambda s: s[:2], word)
+# print(list(a))
+# print(list(b))
+# print(list(c))
+# print(list(d))
+
+# Practice with filter
+# Fill out the rest of the filter functions.
+# You can define additional functions if you need to.
+# (a) range(100) => (0, 3, 6, 9, ...)  (div by 3)
+# (b) range(100) => (0, 5, 10, 15, ...)  (div by 5)
+# (c) range(100) => (0, 15, 30, 45, ...)  (div by 15)
+# (d) range(100) => (1, 2, 4, 7, 8, 11, 13, 14, 16, 17, ...)  (not div by 3 and not div by 5)
+
+a = filter(lambda x: x % 3 ==0, range(100))
+
+b = filter(lambda x: x % 5 ==0, range(100))
+c = filter(lambda x: x % 15 == 0, range(100))
+d = filter(lambda x: x % 3 != 0 and x % 5 != 0 , range(100) )
+
+# print(list(a))
+# print(list(b))
+# print(list(c))
 print(list(d))
