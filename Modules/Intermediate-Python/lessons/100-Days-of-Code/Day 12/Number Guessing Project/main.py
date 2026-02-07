eeb621ac8fe5647e3dd49835f689141eb8a967cd
Arjun Avalakki Mohan <arjun.avalakki@gmail.com>
2026-02-07 09:21:57 +0000

    completed day 12

--- Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 12/Number Guessing Project/main.py
+++ Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 12/Number Guessing Project/main.py
@@ -1,17 +1,35 @@
 import art
 from random import *
 
-print(art.logo)
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
+def set_attempt(diff):
+    if diff == "easy":
+        attempt = 10
+    else:
+        attempt = 5
+    return attempt
 
-difficulty = str(input(f"Choose a difficulty. Type 'easy' or 'hard': ").lower())
+def game():
+    print(art.logo)
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    difficulty = str(input(f"Choose a difficulty. Type 'easy' or 'hard': ").lower())
+    target = randint(1, 100)
+    attempt = set_attempt(difficulty)
 
-comp_random_number = randint(1, 100)
-print(comp_random_number)
+    while attempt > 0:
+        print(f"You have {attempt} attempts remaining to guess the number.")
+        guess = int(input("Make a guess: "))
+        if guess == target:
+            print(f"You got it! The answer was {target}")
+            break
+        elif guess > target:
+            print(f"Too high.")
+            print(f"Guess again")
+        elif guess < target:
+            print(f"Too less.")
+            print(f"Guess again")
+        attempt -= 1
+    else:
+        print(f"You've run out of guesses.")
 
-
-# if difficulty == "easy":
-#     print(f"You have 10 attempts remaining to guess the number.")
-# else:
-#     print(f"You have 5 attempts remaining to guess the number.")
+game()
