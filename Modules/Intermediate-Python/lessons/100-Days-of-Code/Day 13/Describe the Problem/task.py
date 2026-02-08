36a68d4d9bce4319bef1e5c1750b2237e679ec36
Arjun Avalakki Mohan <arjun.avalakki@gmail.com>
2026-02-08 05:29:56 +0000

    day 13

--- Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 13/Describe the Problem/task.py
+++ Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 13/Describe the Problem/task.py
@@ -1,5 +1,5 @@
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
 
@@ -7,6 +7,6 @@ def my_function():
 my_function()
 
 # Describe the Problem - Write your answers as comments:
-# 1. What is the for loop doing?
-# 2. When is the function meant to print "You got it"?
-# 3. What are your assumptions about the value of i?
+# 1. What is the for loop doing? - Its counting values 1 to 19 
+# 2. When is the function meant to print "You got it"? - if the value reaches 20 it should print "You got it"
+# 3. What are your assumptions about the value of i? - it never reaches 20 and so it does not enter the if statement.
--- Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 13/Fix the Errors/task.py
+++ Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 13/Fix the Errors/task.py
@@ -1,3 +1,8 @@
-age = int(input("How old are you?"))
+try:
+    age = int(input("How old are you?"))
+except ValueError:
+    print("Enter a numerical number instead, like 44.")
+    age = int(input("How old are you?"))
+    
 if age > 18:
-print("You can drive at age {age}.")
+    print(f"You can drive at age {age}.")
