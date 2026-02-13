c059eccd1e2dc5090d88d566c12dd8af1f3b0323
Arjun Avalakki Mohan <arjun.avalakki@gmail.com>
2026-02-13 06:43:36 +0000

    Day 16 starter

--- Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 16-100/Where are the rest of the days/main.py
+++ Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 16-100/Where are the rest of the days/main.py
@@ -2,3 +2,20 @@
 # Instead of using templates that I have created for you.
 # It will be another step in your journey as a developer!
 # But don't worry, I will explain how to do everything in the video tutorials on Udemy.
+
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+
+# my_screen = Screen()
+
+# print(my_screen.canvheight)
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.forward(100)
+
+# my_screen.exitonclick()
+
+# python3 -m pip install -U git+https://github.com/prettytable/prettytable
+
+python -m pip --version
