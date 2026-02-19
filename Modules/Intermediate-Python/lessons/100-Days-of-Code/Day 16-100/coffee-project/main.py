a038146dc2b15981d4ab864bc770863638d836cf
Arjun Avalakki Mohan <arjun.avalakki@gmail.com>
2026-02-19 06:56:00 +0000

    Day 16 completed

---
+++ Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 16-100/coffee-project/main.py
@@ -0,0 +1,30 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+import os
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+is_on = True
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino/report/off): ").strip().lower()
+
+    if choice == "off":
+        is_on = False
+        clear_screen()
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        # print(f"{drink}, is being prepared")
+        if drink:
+            if coffee_maker.is_resource_sufficient(drink):
+                print(f"{drink.name} costs {drink.cost}")
+                if money_machine.make_payment(drink.cost):
+                    coffee_maker.make_coffee(drink)
