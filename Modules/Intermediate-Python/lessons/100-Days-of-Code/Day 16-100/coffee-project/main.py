f3a423b64fa301f9bfc8cd926b96913a078422ba
Arjun Avalakki Mohan <arjun.avalakki@gmail.com>
2026-02-19 07:05:50 +0000

    updated day 16 coffee project main file

--- Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 16-100/coffee-project/main.py
+++ Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 16-100/coffee-project/main.py
@@ -12,7 +12,8 @@ menu = Menu()
 
 is_on = True
 while is_on:
-    choice = input("What would you like? (espresso/latte/cappuccino/report/off): ").strip().lower()
+    options = menu.get_items()
+    choice = input(f"What would you like? {options}: ").strip().lower()
 
     if choice == "off":
         is_on = False
