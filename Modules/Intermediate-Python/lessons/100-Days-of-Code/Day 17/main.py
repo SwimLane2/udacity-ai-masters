99f81319d6db934a973ac06d3d730394213a253c
Arjun Avalakki Mohan <arjun.avalakki@gmail.com>
2026-02-20 07:02:40 +0000

    Day 17 completed

---
+++ Modules/Intermediate-Python/lessons/100-Days-of-Code/Day 17/main.py
@@ -0,0 +1,16 @@
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+
+
+
+user_1 = User("001", "Sean")
+print(user_1.followers)
+
+
